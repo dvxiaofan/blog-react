e34bbc32226cfcc2ee5e375a75f01b989af877c8
xiaofan <1353815633@qq.com>
2020-03-01 22:31:48 +0800

    创建广告组件

---
+++ components/Advert.js
@@ -0,0 +1,46 @@
+import '../static/style/components/Advert.css';
+
+const Advert = () => {
+    
+	return (
+		<div className='ad-dv comm-box'>
+			<div>
+				<img
+					src='http://img.devzhangjs.com/img/2020-03-01-140433.png'
+					width='100%'
+				/>
+			</div>
+			<div>
+				<img
+					src='http://img.devzhangjs.com/img/2020-03-01-140433.png'
+					width='100%'
+				/>
+			</div>
+			<div>
+				<img
+					src='http://img.devzhangjs.com/img/2020-03-01-140433.png'
+					width='100%'
+				/>
+			</div>
+			<div>
+				<img
+					src='http://img.devzhangjs.com/img/2020-03-01-140433.png'
+					width='100%'
+				/>
+			</div>
+		</div>
+	);
+};
+
+// const Advert = () => {
+// 	return (
+// 		<div className='comm-box'>
+// 			hello
+// 		</div>
+// 	);
+// };
+
+export default Advert;
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Row, Col, List, Icon } from 'antd';
 import Header from '../components/Header';
 import '../static/style/pages/index.css';
 import Author from '../components/Author';
+import Advert from '../components/Advert';
 
 const Home = () => {
 	const [MyList, setMyList] = useState([
@@ -64,6 +65,7 @@ const Home = () => {
 				</Col>
 				<Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
 					<Author />
+					<Advert />
 				</Col>
 			</Row>
 		</div>
