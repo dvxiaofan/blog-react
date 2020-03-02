0b5a9ee56bb45b56c3c3f43da16b40a103442cd3
xiaofan <1353815633@qq.com>
2020-03-02 11:27:13 +0800

    创建egg.js环境

--- pages/detail.js
+++ pages/detail.js
@@ -98,7 +98,7 @@ const Detail = () => {
 				<Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
 					<Author />
 					<Advert />
-					<Affix offset={5}>
+					<Affix offsetTop={5}>
 						<div className='detail-nav comm-box'>
 							<div className='nav-title'>文章目录</div>
 							<MarkNav
