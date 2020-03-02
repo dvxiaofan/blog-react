f9a5c05f312da98fff0f61d7c217239f8d2ff93d
xiaofan <1353815633@qq.com>
2020-03-02 10:50:37 +0800

    解析静态Markdown

--- pages/detail.js
+++ pages/detail.js
@@ -5,9 +5,47 @@ import Header from '../components/Header';
 import Author from '../components/Author';
 import Footer from '../components/Footer';
 import Advert from '../components/Advert';
+import ReactMarkdown from 'react-markdown';
 import '../static/style/pages/detail.css';
 
 const Detail = () => {
+
+	let markdown =
+		'# P01:课程介绍和环境搭建\n' +
+		'[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+		'> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+		'**这是加粗的文字**\n\n' +
+		'*这是倾斜的文字*`\n\n' +
+		'***这是斜体加粗的文字***\n\n' +
+		'~~这是加删除线的文字~~ \n\n' +
+		'`console.log(111)` \n\n' +
+		'# p02:来个Hello World 初始Vue3.0\n' +
+		'> aaaaaaaaa\n' +
+		'>> bbbbbbbbb\n' +
+		'>>> cccccccccc\n' +
+		'***\n\n\n' +
+		'# p03:Vue3.0基础知识讲解\n' +
+		'> aaaaaaaaa\n' +
+		'>> bbbbbbbbb\n' +
+		'>>> cccccccccc\n\n' +
+		'# p04:Vue3.0基础知识讲解\n' +
+		'> aaaaaaaaa\n' +
+		'>> bbbbbbbbb\n' +
+		'>>> cccccccccc\n\n' +
+		'#5 p05:Vue3.0基础知识讲解\n' +
+		'> aaaaaaaaa\n' +
+		'>> bbbbbbbbb\n' +
+		'>>> cccccccccc\n\n' +
+		'# p06:Vue3.0基础知识讲解\n' +
+		'> aaaaaaaaa\n' +
+		'>> bbbbbbbbb\n' +
+		'>>> cccccccccc\n\n' +
+		'# p07:Vue3.0基础知识讲解\n' +
+		'> aaaaaaaaa\n' +
+		'>> bbbbbbbbb\n' +
+		'>>> cccccccccc\n\n' +
+		'``` var a=11; ```';
+
 	return (
 		<div className='container'>
 			<Head>
@@ -37,13 +75,22 @@ const Detail = () => {
 						<div className='detail-title'>
 							JS 基础知识点及常考面试题(一)
 						</div>
-                        <div className='list-icon center'>
-                            <span><Icon type='calendar' /> 2020-03-01</span>
-                            <span><Icon type='folder' /> JavaScript</span>
-                            <span><Icon type='fire' /> 5565</span>
-                        </div>
-                        <div className='detail-content'>
-                            Markdown 内容解析
+						<div className='list-icon center'>
+							<span>
+								<Icon type='calendar' /> 2020-03-01
+							</span>
+							<span>
+								<Icon type='folder' /> JavaScript
+							</span>
+							<span>
+								<Icon type='fire' /> 5565
+							</span>
+						</div>
+						<div className='detail-content'>
+                            <ReactMarkdown
+                                source={markdown}
+                                escapeHtml={false}
+                            />
                         </div>
 					</div>
 				</Col>
