0aba86511b0f246163f33eba9b39f6af83a77fda
xiaofan <1353815633@qq.com>
2020-03-05 15:05:40 +0800

    从数据库获取首页列表数据

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,15 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import { Row, Col, List, Icon } from 'antd';
+import axios from 'axios';
 import Header from '../components/Header';
 import Author from '../components/Author';
 import Advert from '../components/Advert';
 import Footer from '../components/Footer';
 import '../static/style/pages/index.css';
 
-const Home = () => {
-	const [MyList, setMyList] = useState([
-		{
-			title: 'JS 基础知识点及常考面试题(一',
-            context:
-				'JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，'
-		},
-		{
-			title: 'JS 基础知识点及常考面试题(二',
-            context:
-				'JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，'
-		},
-		{
-			title: 'JS 基础知识点及常考面试题(三',
-            context:
-				'JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，'
-		},
-		{
-			title: 'JS 基础知识点及常考面试题(四',
-            context:
-				'JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，'
-		},
-		{
-			title: 'JS 基础知识点及常考面试题(五',
-            context:
-				'JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，JS 对于每位前端开发都是必备技能，在小册中我们也会有多个章节去讲述这部分的知识。首先我们先来熟悉下 JS 的一些常考和容易混乱的基础知识点原始（Primitive）类型涉及面试题：原始类型有哪几种？null 是对象嘛？在 JS 中，存在着 6 种原始值，分别是：首先原始类型存储的都是值，是没有函数可以调用的，比如 undefined.toString()此时你肯定会有疑问，这不对呀，'
-		}
-	]);
+const Home = (list) => {
+	const [MyList, setMyList] = useState(list.data);
 
 	return (
 		<div className='container'>
@@ -55,11 +30,11 @@ const Home = () => {
 							<List.Item>
 								<div className='list-title'>{item.title}</div>
 								<div className='list-icon'>
-									<span><Icon type='calendar'/> 2020-03-01</span>
-									<span><Icon type='folder'/> JavaScript</span>
-									<span><Icon type='fire'/> 7676</span>
+									<span><Icon type='calendar'/> {item.addTime}</span>
+									<span><Icon type='folder'/> {item.typeName}</span>
+									<span><Icon type='fire'/> {item.view_count}</span>
 								</div>
-								<div className='list-context'>{item.context}</div>
+								<div className='list-context'>{item.introduce}</div>
 							</List.Item>
 						)}
 					/>
@@ -73,5 +48,17 @@ const Home = () => {
 		</div>
 	);
 };
+
+Home.getInitialProps = async () => {
+	const promise = new Promise((resolve) => {
+		axios('http://127.0.0.1:7001/default/getArticleList')
+		.then((res) => {
+			console.log('res: ', res.data);
+			resolve(res.data)
+		})
+	})
+
+	return await promise
+}
 
 export default Home;
