7069f2edb73edeb5f47531e86505b5e8878aef62
xiaofan <1353815633@qq.com>
2020-03-05 18:37:29 +0800

    修改部分样式

--- pages/detail.js
+++ pages/detail.js
@@ -11,35 +11,34 @@ import 'markdown-navbar/dist/navbar.css';
 import '../static/style/pages/detail.css';
 
 import marked from 'marked';
-import highlight from 'highlight.js';
+import hljs from 'highlight.js';
 import 'highlight.js/styles/monokai-sublime.css';
 
-const Detail = (props) => {
-
-	const renderer = new marked.Renderer()
+const Detail = props => {
+	const renderer = new marked.Renderer();
 
 	marked.setOptions({
-		renderer,
+		renderer: renderer,
 		gfm: true,
-		pedantic: false,	// 容错
-		sanitize: false,	// 是否忽略html
-		tables: true,		// 表格	-- gfm 为 true 才有效
-		breaks: false,		// 换行符 -- gfm 为 true 才有效
-		smartLists: true,	// 自动渲染列表
+		pedantic: false, // 容错
+		sanitize: false, // 是否忽略html
+		tables: true, // 表格	-- gfm 为 true 才有效
+		breaks: false, // 换行符 -- gfm 为 true 才有效
+		smartLists: true, // 自动渲染列表
 		highlight: function(code) {
 			// 根据传入代码检测显示方法
-			return highlight.highlightAuto(code)
-		}
-	})
+			return hljs.highlightAuto(code).value;
+		},
+	});
 
-	let html = marked(props.articleContent)
+	let html = marked(props.articleContent);
 
 	return (
 		<div className='container'>
 			<Head>
-				<title>详情</title>{' '}
+				<title> 详情 </title>
 				<link rel='icon' href='/public/favicon.ico' />
-			</Head>{' '}
+			</Head>
 			<Header />
 			<Row className='comm-main' type='flex' justify='center'>
 				<Col
@@ -53,16 +52,14 @@ const Detail = (props) => {
 					<div className='bread-div'>
 						<Breadcrumb>
 							<Breadcrumb.Item>
-								<a href='/'>首页</a>
+								<a href='/'> 首页 </a>
 							</Breadcrumb.Item>
-							<Breadcrumb.Item>博文列表</Breadcrumb.Item>
-							<Breadcrumb.Item>XXX</Breadcrumb.Item>
+							<Breadcrumb.Item> 博文列表 </Breadcrumb.Item>
+							<Breadcrumb.Item> XXX </Breadcrumb.Item>
 						</Breadcrumb>
 					</div>
 					<div>
-						<div className='detail-title'>
-							{props.title}
-						</div>
+						<div className='detail-title'> {props.title} </div>
 						<div className='list-icon center'>
 							<span>
 								<Icon type='calendar' /> {props.addTime}
@@ -74,11 +71,12 @@ const Detail = (props) => {
 								<Icon type='fire' /> {props.viewCount}
 							</span>
 						</div>
-						<div className='detail-content'
-							dangerouslySetInnerHTML={{__html: html}}
-						>
-							
-						</div>
+						<div
+							className='detail-content'
+							dangerouslySetInnerHTML={{
+								__html: html
+							}}
+						></div>
 					</div>
 				</Col>
 				<Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
@@ -86,7 +84,7 @@ const Detail = (props) => {
 					<Advert />
 					<Affix offsetTop={5}>
 						<div className='detail-nav comm-box'>
-							<div className='nav-title'>文章目录</div>
+							<div className='nav-title'> 文章目录 </div>
 							<MarkNav
 								className='article-menu'
 								source={html}
@@ -102,19 +100,19 @@ const Detail = (props) => {
 	);
 };
 
-Detail.getInitialProps = async(context) => {
+Detail.getInitialProps = async context => {
+	let id = context.query.id;
 
-	let id = context.query.id
+	const promise = new Promise(resolve => {
+		axios('http://127.0.0.1:7001/default/getArticleById/' + id).then(
+			res => {
+				// console.log('res: ', res.data);
+				resolve(res.data.data[0]);
+			}
+		);
+	});
 
-	const promise = new Promise((resolve => {
-		axios('http://127.0.0.1:7001/default/getArticleById/' + id)
-		.then(res => {
-			// console.log('res: ', res.data);
-			resolve(res.data.data[0])
-		})
-	}))
-
-	return await promise
-}
+	return await promise;
+};
 
 export default Detail;
